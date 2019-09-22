ccb28a803a2a97cd7abbf8d9e85bbfa472980b84
muhamadirsyad <55641656+muhamadirsyad@users.noreply.github.com>
2019-09-22 08:03:23 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Rumus Segitiga, Lingkaran, Jajargenjang </h1>
+</body>
+</html>
+<?php
+$x=20;
+$y=30;
+echo"sebuah segitiga dengan keterangan sebagai berikut";
+echo "<br>";
+echo"panjang: ". $x;
+echo "<br>";
+echo "tinggi: ". $y;
+echo "<br>";
+echo "maka luasnya= ". $x*$y/2;
+echo "<br>"
+?>
+
+<?php
+$a=20;
+$c=10;
+$b=3.14;
+echo "<br>";
+echo "dua buah lingkaran dengan jari-jari sepanjang: ". $a. " dan " .$c;
+echo "<br>";
+echo "maka luas lingkaran tersebut ";
+echo "<br>";
+echo "luas lingkaran dengan jari-jari ". $a. "= ". $b*$a*$a;
+echo "<br>";
+echo "luas lingkaran dengan jari-jari ". $c. "= ". $b*$c*$c;
+echo "<br>";
+?>
+
+<?php
+$x=22;
+$y=12;
+echo "<br>";
+echo "sebuah jajargenjang dengan panjang alas: ". $x ." dan tinggi: ". $y;
+echo "<br>";
+echo "maka luas yang diperoleh= ". $x*$y;
+?>
